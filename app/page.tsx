d6eb30bc444d2f5faea9bd7c03a87485ce6d7591
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-08-12 13:29:07 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
-import Image from "next/image";
+import BentoGridDemo from "@/components/ui/Grid";
 import { Hero } from "../components/Hero";
-import { GridBackgroundDemo} from "@/components/ui/GridBackground";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center item-center flex-col overflow-hidden mx-auto sm:px-10">
-      <div className="max-w-7xl w-full">
+      <div className="max-w-6xl w-full">
+        <FloatingNav className="" navItems={[
+          {name:'Home', link:'/'},
+          {name:'About', link:'/about'}
+        ]}/>
         <Hero/> 
+        <BentoGridDemo />
       </div>
     </main>
   );
