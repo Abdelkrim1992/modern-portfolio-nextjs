7484403a6ac160c53f941a66fdb0d5dc2acc600a
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-08-14 13:24:33 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center item-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-6xl w-full">
-        <FloatingNav className="" navItems={[
+        <FloatingNav className="max-w-fit" navItems={[
           {name:'Home', link:'/'},
           {name:'About', link:'/about'}
         ]}/>
