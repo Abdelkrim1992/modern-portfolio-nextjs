3036314f04792acf9d2ea583da2390a9b1283e51
Abdelkrim Salaghe <abdelkrimsalaghe04@gmail.com>
2025-08-15 17:38:13 +0100

    update project files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,14 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-import { ThemeProvider } from "next-themes";
+import { Inter } from "next/font/google";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import "./globals.css";
+import { ThemeProvider } from "./provider";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Abdelkrim Modern Portfolio",
-  description: "Modern Porfolio Next.js",
+  title: "Abdelkrim Salaghe Portfolio",
+  description: "Modern Portfolio",
 };
 
 export default function RootLayout({
@@ -25,14 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange>
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+      </head>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
--- lib/clipboard-config.ts
+++
@@ -1,5 +0,0 @@
-export const clipboardConfig = {
-  text: "Copy my email address",
-  value: "abdelkrimsalaghe04@gmail.com",
-  copiedText: "Email copied",
-}; 
